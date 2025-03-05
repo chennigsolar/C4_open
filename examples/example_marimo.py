fc93909b90328614f2a17be5ae1537a3302d3836
carstenhennigberlin <carsten_hennig@gmx.de>
2025-03-05 14:08:05 +0100

    Cyclic load factor calculation added to marimo notebook

--- examples/example_marimo.py
+++ examples/example_marimo.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.10.14"
+__generated_with = "0.11.13"
 app = marimo.App(width="medium")
 
 
@@ -16,10 +16,22 @@ def _():
     from c4_open.cables import Cable
     from c4_open.project import Project
     from c4_open.arrangements import create_arrangement
+    from c4_open.load_profile import LoadProfile
+    from c4_open.cyclic_rating_factors import FactorM
     import c4_open
     import pandas as pd
     import matplotlib.pyplot as plt
-    return Cable, Project, c4_open, create_arrangement, mo, pd, plt
+    return (
+        Cable,
+        FactorM,
+        LoadProfile,
+        Project,
+        c4_open,
+        create_arrangement,
+        mo,
+        pd,
+        plt,
+    )
 
 
 @app.cell
@@ -33,14 +45,6 @@ def _(mo):
     name = mo.ui.text(value='Example', label='Project name')
     name
     return (name,)
-
-
-@app.cell
-def _(mo):
-    calculation_case = mo.ui.dropdown(
-        options=["dc_sc", "ac_sc", "ac_mc"], value="dc_sc", label='Calculation case')
-    calculation_case
-    return (calculation_case,)
 
 
 @app.cell
@@ -103,7 +107,6 @@ def _(
     N,
     Project,
     cable_type,
-    calculation_case,
     create_arrangement,
     d_clear,
     deltatheta_x,
@@ -114,7 +117,7 @@ def _(
     theta_amb,
 ):
     project_data = {'name': name.value, 
-                    'calc_case': calculation_case.value, 
+                    'calc_case': 'dc_sc', 
                     'cable_type':cable_type.value,
                     'L': L.value,
                     'N': N,
@@ -132,12 +135,22 @@ def _(
 
 
 @app.cell
+def _(mo):
+    mo.md(r"""## Arrangement plot""")
+    return
+
+
+@app.cell
 def _(plt, xx, yy):
     plt.scatter(xx, yy, marker='o', s = 3)
-    plt.xlim(-1, 1)
-    plt.ylim(-1.5, 0.7)
+    y_max = 0
+    y_min = min(yy) - 0.2
+    x_min = min(xx) - 0.2
+    x_max = max(xx) + 0.2
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
     plt.gca()
-    return
+    return x_max, x_min, y_max, y_min
 
 
 @app.cell
@@ -154,13 +167,141 @@ def _(cable):
 
 @app.cell
 def _(mo):
-    mo.md("""## Resulting current carrying capacity""")
+    mo.md("""## Steady-state Current carrying capacity""")
     return
 
 
 @app.cell
-def _(mo, result):
-    mo.output.replace(f'{round(result['I (with dry zone)'], 1)} A')
+def _(result):
+    (f'Current carrying capacity with 100% load factor: {round(result['I (with dry zone)'], 1)} A')
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Cyclic Rating Factor""")
+    return
+
+
+@app.cell
+def _():
+    ### Load profile
+    return
+
+
+@app.cell
+def _(mo):
+    path = mo.ui.file_browser(label='Select .xlsx with load profile', filetypes=['.xlsx'], multiple=False)
+    path
+    return (path,)
+
+
+@app.cell
+def _(path):
+    print(path.path())
+    return
+
+
+@app.cell
+def _(LoadProfile, path):
+    if path.path():
+        load_profile = LoadProfile(path.path())
+    else:
+        load_profile = None
+    return (load_profile,)
+
+
+@app.cell
+def _(load_profile):
+    if load_profile:
+        res = load_profile.get_results()
+    else:
+        res = None
+    return (res,)
+
+
+@app.cell
+def _(load_profile, res):
+    if load_profile:
+        plot = res.plot()
+    else:
+        plot = None
+    return (plot,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Load profile plot""")
+    return
+
+
+@app.cell
+def _(plot):
+    plot
+    return
+
+
+@app.cell
+def _(Cable, FactorM, load_profile, path, project):
+    updated_cable = None
+    updated_result = None
+    if path.path():
+        project.get_delta_theta_x(load_profile)
+        updated_cable = Cable(project)  # create cable with new delta_theta_x value acc. to load_profile
+        updated_result = updated_cable.get_result()
+        M = FactorM(updated_cable, load_profile)
+    return M, updated_cable, updated_result
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Interim results""")
+    return
+
+
+@app.cell
+def _(updated_result):
+    updated_result
+    return
+
+
+@app.cell
+def _(M, path):
+    if path.path():
+        M_results = M.get_result()
+    else:
+        M_results = None
+    return (M_results,)
+
+
+@app.cell
+def _(M_results):
+    M_results
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Cyclic Load Current Carrying Capacity""")
+    return
+
+
+@app.cell
+def _(M_results, updated_result):
+    final_result = None
+    result_string = '-'
+    if M_results:
+        if M_results['M_1']:
+            final_result = M_results['M_1'] * updated_result['I (with dry zone)']
+        else: 
+            final_result = M_results['M (no dry zone)'] * updated_result['I (no dry zone)']
+        result_string = f'Current carrying capacity under cyclic load: {round(final_result, 1)} A'
+    return final_result, result_string
+
+
+@app.cell
+def _(result_string):
+    result_string
     return
 
 
