6d15c68289c8844298d8735480c15d592d2d743a
carstenhennigberlin <carsten_hennig@gmx.de>
2026-01-14 18:19:17 +0100

    New cable type NYY 1x50 added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='c4_open',
-    version='0.1.6',
+    version='0.1.7',
     author='Carsten Hennig',
     author_email='carsten_hennig@gmx.de',
     description='A package for current carrying capacity calculation of cables',
